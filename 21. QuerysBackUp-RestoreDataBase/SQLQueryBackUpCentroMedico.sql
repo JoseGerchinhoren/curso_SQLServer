8efc3913fdd568acc79b677c2da64a1a0494ab01
unknown <josegerchinhoren97@gmail.com>
2023-09-01 22:52:29 -0300

    Hasta creacion de backup

---
+++ 21. QuerysBackUp-RestoreDataBase/SQLQueryBackUpCentroMedico.sql
@@ -0,0 +1,3 @@
+BACKUP DATABASE CentroMedico1
+TO DISK = 'D:\José\Ciencia de datos\Curso SQL Server\curso_SQLServer\BackUpSQL\CentroMedico1.bak'
+WITH NO_COMPRESSION, NAME='CentroMedico1'
---
+++ 21. QuerysBackUp-RestoreDataBase/SQLQueryBackUpCentroMedico_Version2.sql
@@ -0,0 +1,13 @@
+-- Para que se vaya guardando con fecha y hora el backup:
+declare @fecha char(12)
+declare @path varchar(100)
+declare @name varchar(20)
+
+-- print convert(char(8), getdate(), 112)
+set @fecha = convert (char(8), getdate(), 112) + replace(convert(char(5), getdate(), 108),':','')
+set @path = 'D:\José\Ciencia de datos\Curso SQL Server\curso_SQLServer\BackUpSQL\CentroMedico'+@fecha+'.bak'
+set @name = 'CentroMedico'+@fecha
+
+BACKUP DATABASE CentroMedico
+TO DISK = @path
+WITH NO_COMPRESSION, NAME=@name
