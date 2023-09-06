7a49dd5b8fced7c04424d4c7d2c825b981212a52
unknown <josegerchinhoren97@gmail.com>
2023-09-06 16:50:17 -0300

    Hasta creacion de Usuarios y Roles

---
+++ 23. QuerysAdministraciónUsuarios-Roles-Schemas-Permisos/SQLQueryPruebaPermisosUsuarioCreado.sql
@@ -0,0 +1,5 @@
+-- El usuario "Jose" esta configurado para no tener permisos para insertar valores en la tabla "Especialidad"
+
+SELECT * FROM Especialidad
+
+INSERT INTO Especialidad VALUES ('Nutricionista')
